c02cd009c2e167d1c1daebf6b74f259ea48bf5ac
Eisner López Acevedo <elopeza@ucatolica.ac.cr>
2018-02-21 16:24:44 -0600

    Inicial
    Commit

---
+++ app/src/main/java/com/example/eisne/inventror/Interfaces/IFragments.java
@@ -0,0 +1,10 @@
+package com.example.eisne.inventror.Interfaces;
+
+import com.example.eisne.inventror.BlankFragment;
+
+/**
+ * Created by eisne on 20/2/2018.
+ */
+
+public interface IFragments extends BlankFragment.OnFragmentInteractionListener{
+}
